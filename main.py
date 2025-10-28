f3bdf860cce97e4747f3fb505d03e548063feae3
azimumarov2001 <team.lord990@gmail.com>
2025-10-28 07:43:56 +0500

    main.py

--- main.py
+++ main.py
@@ -3,79 +3,113 @@ from pydantic import BaseModel
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine, Column, Integer, String
 
-DATABASE_URL = 'sqlite:///database.db'
+DATABASE_URL = "sqlite:///database.db"
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 app = FastAPI()
 
 
-class Movie(Base):
-    __tablename__ = 'movies'
+class Book(Base):
+    __tablename__ = "books"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False, unique=True)
-    genres = Column(String, nullable=False)
-    rating = Column(Integer, nullable=False)
+    title = Column(String)
+    author = Column(String)
+    year = Column(Integer)
 
 
-class CreateMovie(BaseModel):
+class CreateBook(BaseModel):
     title: str
-    genres: str
-    rating: int
-
-
-class UpdateMovie(BaseModel):
-    title: str | None = None
-    genres: str | None = None
-    rating: int | None = None
+    author: str
+    year: int
 
 
 Base.metadata.create_all(bind=engine)
 
 
-@app.post('/movies')
-def create_movie(movie: CreateMovie):
+@app.post("/books")
+def create_book(book: CreateBook):
     db = SessionLocal()
     try:
-        if db.query(Movie).filter(Movie.title == movie.title).first():
-            raise HTTPException(status_code=400, detail='Movie already exists')
-        new_movie = Movie(title=movie.title, genres=movie.genres, rating=movie.rating)
-        db.add(new_movie)
+        if db.query(Book).filter(Book.title == book.title).first():
+            raise HTTPException(status_code=400, detail="Book already exists")
+        new_book = Book(title=book.title, author=book.author, year=book.year)
+        db.add(new_book)
         db.commit()
-        db.refresh(new_movie)
-        return new_movie
+        db.refresh(new_book)
+        return new_book
     finally:
         db.close()
 
 
-@app.get('/movies')
-def read_movies():
-    db = SessionLocal()
-    get_movies = db.query(Movie).all()
-    db.close()
-    return get_movies
-
-
-@app.get('/movies/{movie_id}')
-def read_movie1(movie_id: int):
-    db = SessionLocal()
-    movie = db.query(Movie).filter(Movie.id == movie_id).first()
-    db.close()
-    if not movie:
-        db.close()
-        raise HTTPException(status_code=404, detail='Movie not found')
-    return movie
-
-
-@app.delete('/movies/{movie_id}')
-def delete_movie(movie_id: int):
+@app.get("/books")
+def read_books():
     db = SessionLocal()
     try:
-        deleted_movie = db.query(Movie).filter(Movie.id == movie_id).first()
-        if not deleted_movie:
-            raise HTTPException(status_code=404, detail='Movie not found')
-        db.delete(deleted_movie)
+        books = db.query(Book).all()
+        return books
+    finally:
+        db.close()
+
+
+@app.get("/books/{book_id}")
+def read_book(book_id: int):
+    db = SessionLocal()
+    try:
+        book1 = db.query(Book).filter(Book.id == book_id).first()
+        if not book1:
+            raise HTTPException(status_code=404, detail="Book not found")
+        return book1
+    finally:
+        db.close()
+
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int):
+    db = SessionLocal()
+    try:
+        delete_book = db.query(Book).filter(Book.id == book_id).first()
+        if not delete_book:
+            raise HTTPException(status_code=404, detail="Book not found")
+        db.delete(delete_book)
         db.commit()
-        return {'message': 'Movie deleted'}
+        return delete_book
+    finally:
+        db.close()
+
+
+@app.put("/books/{book_id}")
+def update_book(book_id: int, book: CreateBook):
+    db = SessionLocal()
+    try:
+        db_book = db.query(Book).filter(Book.id == book_id).first()
+        if not db_book:
+            raise HTTPException(status_code=404, detail="Book not found")
+        db_book.title = book.title
+        db_book.author = book.author
+        db_book.year = book.year
+        db.commit()
+        db.refresh(db_book)
+        return db_book
+    finally:
+        db.close()
+
+
+@app.patch("/books/{book_id}")
+def update_book(book_id: int, book: CreateBook):
+    db = SessionLocal()
+    try:
+        patch_book = db.query(Book).filter(Book.id == book_id).first()
+        if not patch_book:
+            raise HTTPException(status_code=404, detail="Book not found")
+        if patch_book.title is not None:
+            patch_book.title = book.title
+        if patch_book.author is not None:
+            patch_book.author = book.author
+        if patch_book.year is not None:
+            patch_book.year = book.year
+        db.commit()
+        db.refresh(patch_book)
+        return patch_book
     finally:
         db.close()
